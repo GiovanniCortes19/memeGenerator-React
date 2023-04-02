eab49825504a14dd095bb5fc4fff83cdf7e46446
GiovanniCortes <giocortes19@gmail.com>
2023-04-02 15:04:31 -0400

    initialized style to have none of default styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Navbar />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      
     </div>
   )
 }
