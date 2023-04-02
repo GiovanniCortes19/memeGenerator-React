e559734ee1f197268affcdf52e89ab02b3c8cd13
GiovanniCortes <giocortes19@gmail.com>
2023-04-02 16:42:07 -0400

    added meme image locally

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
+import memeIcon from './meme.png'
 
 export default function Navbar (){
     return (
         <nav className="navbar">
             <div className="nameLogo">
-                <img className="logo-img" src="https://thenounproject.com/api/private/icons/866567/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABkKdixLm1DOz-arjoqd3ejbYIuW0sRrM6HBnWa4GoQ_j-t6ZhryVhd1vKhIMUN1oUIcqsKoOUKgoEGjgm12oCJhn0asw%3D%3D" alt="meme-face" />
+                <img className='logo-img' src={memeIcon} alt="meme-img" />
                 <h1 className="logo-name">Meme Generator</h1>
             </div>
             <p>React Course: Project 3</p>
