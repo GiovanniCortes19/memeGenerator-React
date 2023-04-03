241a683cb5c6141fcf2183f1b6af05573b29529c
GiovanniCortes <giocortes19@gmail.com>
2023-04-02 21:41:18 -0400

    added classNames to form elements

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,8 +2,8 @@ export default function Meme(){
     return (
         <main>
             <form className="form" action="">
-                <input type="text" />
-                <input type="text" />
+                <input className="form-input" placeholder="Top text" type="text" />
+                <input className="form-input" placeholder="Bottom text" type="text" />
                 <button className="form-btn">Get a new meme image  🖼</button>
             </form>
         </main>
