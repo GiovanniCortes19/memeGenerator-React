902157607b2fafba772adb514449f1dd7d33e47f
GiovanniCortes <giocortes19@gmail.com>
2023-04-04 16:32:53 -0400

    created and implemented getMemeImage() in Meme component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,22 +1,33 @@
 import memesData from "./memesData"
+import { useState } from "react"
 
 export default function Meme(){
     // creating the State
-    const [meme, setMeme] = React.useState({
+    const [meme, setMeme] = useState({
         topText: "Top Text",
         bottomText: "Bottom Text",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: memesArray[randomNumber].url
+        }))
+    }
 
     return (
         <main>
             <form className="form" action="">
                 <input className="form-input" placeholder="Top text" type="text" />
                 <input className="form-input" placeholder="Bottom text" type="text" />
-                <button className="form-btn">Get a new meme image  🖼</button>
+                <button className="form-btn" onClick={getMemeImage} >Get a new meme image  🖼</button>
             </form>
+            <img className="meme-img" src={meme.randomImage} alt="meme-img" />
         </main>
     )
 }
